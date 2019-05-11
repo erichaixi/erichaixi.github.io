59c10e5df4e02eb5dfe2f47b92f659be2f3923db
erichaixi <erichaixi@yahoo.com>
2019-05-11 18:29:45 -0500

    Improved aesthetics a bit

--- billsplitter/billsplitter.js
+++ billsplitter/billsplitter.js
@@ -12,10 +12,15 @@ function update_tip(val) {
 	update_all();
 }
 
+function update_people(val) {
+	document.getElementById("num_people_count").innerHTML = val;
+	update_all();
+}
+
 function update_all() {
 	let tip_pct = parseInt(document.getElementById("tip-pct").innerHTML.slice(0, -1));
-	console.log("tip %: " + tip_pct);
 
+	// Get current total, pre-tax
 	let curr_total = document.getElementById("total-amount").value;
 	if (curr_total.length > 0) {
 		curr_total = parseFloat(curr_total);
@@ -23,8 +28,9 @@ function update_all() {
 	else {
 		curr_total = 0;
 	}
-	console.log("Total: " + curr_total);
 
+	// Tip amount + grand total
+	let grand_total = 0;
 	if (curr_total == 0) {
 		console.log("Empty total");
 		document.getElementById("tip-amt").innerHTML = "$0.00";
@@ -35,8 +41,13 @@ function update_all() {
 		document.getElementById("tip-amt").innerHTML = "$" + tip;
 		console.log("Tip amount: " + tip);
 
-		let grand_total = (tip + curr_total).toFixed(2);
+		grand_total = (tip + curr_total).toFixed(2);
 		console.log("Grand total: " + grand_total);
 		document.getElementById("grand-total-amt").innerHTML = "$" + grand_total;
 	}
+
+	// $ per person
+	let num_people = parseInt(document.getElementById("num_people_count").innerHTML);
+	let cost_per_person = (parseFloat(grand_total) / num_people).toFixed(2);
+	document.getElementById("split_cost").innerHTML = "$" + cost_per_person;
 }
