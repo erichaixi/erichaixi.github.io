7ae934ad1a90e3b0762881eaac91dd626804308b
erichaixi <erichaixi@yahoo.com>
2019-05-16 00:44:01 -0500

    Updated Reversi
    - Completed Info Display section on the right side

--- reversi/src/index.js
+++ reversi/src/index.js
@@ -4,20 +4,42 @@ import './index.css';
 import { isUndefined } from 'util';
 
 function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
+    if (props.value == "B") {
+        return (
+            <button className="black-square square" onClick={props.onClick}>
+            </button>
+        );
+    }
+    else if (props.value == "W") {
+        return (
+            <button className="white-square square" onClick={props.onClick}>
+            </button>
+        );
+    }
+    else {
+        return (
+            <button className="square" onClick={props.onClick}>
+                 {props.value}
+            </button>
+        );
+    }
 }
+
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(81).fill(null),
-            turn: "O"
+            turn: "B",
+            numBlack: 2,
+            numWhite: 2,
+            numRemaining: 60
         };
+        this.state.squares[27] = "W";
+        this.state.squares[28] = "B";
+        this.state.squares[35] = "B";
+        this.state.squares[36] = "W";
     }
 
     renderSquare(i) {
@@ -30,57 +52,130 @@ class Board extends React.Component {
     }
 
     handleClick(i) {
-        if (this.state.squares[i] == null && this.state.turn[0] != "W") {
+        /*
+         For each direction (up, down, left, right, upleft, upright, downleft, downright),
+         see if any pieces will be captured.
+
+        If pieces are captured, process those captures.
+
+        If no pieces would be captured, change nothing and don't render as this is invalid move.
+        */
+        let isValidMove = false;
+        if (this.state.squares[i] == null) {
             const squares = this.state.squares.slice();
             squares[i] = this.state.turn;
-            
+
+            if (this.state.turn == "B") {
+                const numBlack = this.state.numBlack + 1;
+                this.setState({ numBlack: numBlack })
+            }
+            else {
+                const numWhite = this.state.numWhite + 1;
+                this.setState({ numWhite: numWhite })
+            }
+
+            const numRemaining = this.state.numRemaining - 1;
+            this.setState({ numRemaining: numRemaining })
+
             this.setState({ squares: squares });
+
+            isValidMove = true;
+        }
+
+        if (isValidMove) {
+            this.alternateTurn();
         }
     }
 
     alternateTurn() {
-        if (this.state.turn[0] == "W") {
-            return;
-        }
-        else if (this.state.turn == "X") {
-            this.state.turn = "O";
+        //if (this.state.turn[0] == "W") {
+        //    return;
+        //}
+        if (this.state.turn == "W") {
+            this.state.turn = "B";
         }
         else {
-            this.state.turn = "X";
+            this.state.turn = "W";
         }
     }
 
     render() {
-        this.alternateTurn();
-        let status = "";
-        if (this.state.turn[0] == "W") {
-            status = this.state.turn;
-        }
-        else {
-            status = 'Next player: ' + this.state.turn;
-        }
-
-
-        
         let rows = [];
 
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 8; i++) {
             let row_items = [];
-            for (let j = 0; j < 9; j++) {
-                row_items.push(<span>{this.renderSquare(i * 9 + j)}</span>)
+            for (let j = 0; j < 8; j++) {
+                row_items.push(<span>{this.renderSquare(i * 8 + j)}</span>)
             }
             rows.push(<div className="board-row">{row_items}</div>)
         }
 
-        return (
-            <div>
-                
-                <div className="game-board">
-                    {rows}
+        if (this.state.turn == "B") {
+            return (
+                <div>
+                    <div className="game">
+                        <div className="game-board">
+                            <div className="game-board">
+                                {rows}
+                            </div>
+                        </div>
+                        <div className="game-info">
+                            <div>
+                                <div class="score-title">Scores</div>
+                                <div className="score-display-highlighted">
+                                    <div className="black-piece-display"></div>
+                                    <div className="black-counter">{this.state.numBlack}</div>
+                                </div>
+                                <div className="score-display">
+                                    <div className="white-piece-display"></div>
+                                    <div className="white-counter">{this.state.numWhite}</div>
+                                </div>
+                                <div class="remaining-title">Remaining Pieces</div>
+                                <div className="remaining-display">
+                                    <div className="remaining-black-piece"></div>
+                                    <div className="remaining-white-piece"></div>
+                                    <div className="remaining-counter">{this.state.numRemaining}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                
-            </div>
-        );
+            );
+        }
+        else if (this.state.turn == "W") {
+            return (
+                <div>
+                    <div className="game">
+                        <div className="game-board">
+                            <div className="game-board">
+                                {rows}
+                            </div>
+                        </div>
+                        <div className="game-info">
+                            <div>
+                                <div class="score-title">Scores</div>
+                                <div className="score-display">
+                                    <div className="black-piece-display"></div>
+                                    <div className="black-counter">{this.state.numBlack}</div>
+                                </div>
+                                <div className="score-display-highlighted">
+                                    <div className="white-piece-display"></div>
+                                    <div className="white-counter">{this.state.numWhite}</div>
+                                </div>
+                                <div class="remaining-title">Remaining Pieces</div>
+                                <div className="remaining-display">
+                                    <div className="remaining-black-piece"></div>
+                                    <div className="remaining-white-piece"></div>
+                                    <div className="remaining-counter">{this.state.numRemaining}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            );
+        }
     }
 }
 
@@ -91,17 +186,9 @@ class Game extends React.Component {
                 <h1 className="Title">
                     Reversi
                 </h1>
-                <div className="game">   
-                    
-                    <div className="game-board">
-                        <Board />
-                    </div>
-                    <div className="game-info">
-                        <div>
-                            Info
-                        </div>
-                    </div>
-                </div>
+   
+                <Board />
+
             </div>
         );
     }
